98dce676f40fac8bbe53a7b823804fc5cd3af738
Joshua Allan <joshuanallan@gmail.com>
2026-03-29 21:17:13 -0600

    Push 245: reload page on service worker update so PWA always gets latest code

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// Reload the page whenever a new service worker takes control so users always get the latest version
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.addEventListener('controllerchange', () => {
+    window.location.reload();
+  });
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
